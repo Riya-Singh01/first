075fa0ed38c40906a05e5bcf4879b5595ed21640
Riya singh <riyasingh56192@gmail.com>
2025-12-19 08:28:36 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,21 @@
+let score = 26;
+
+console.log(typeof score);
+
+let age = "riya";
+let valueInNumber = Number(age);
+console.log(typeof valueInNumber ); // here it print number but clearly shows that it is not a number
+
+console.log(valueInNumber);// here it print nan
+
+//"33" // easily converted into number... value:-33
+//"66anb" //  its type show that number but not converted... value:-NaN(Not a number)
+
+let str = 89;
+let string1 = String(str);
+console.log(string1);
+console.log(typeof string1);
+
+
+
+
