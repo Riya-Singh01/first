01279c6f5eb22311be443130b61618210f8842b0
Riya singh <riyasingh56192@gmail.com>
2025-12-19 04:18:08 +0000

    Variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553;
+let accountEmail = "riya123@gmail.com";
+var accountPassword = "123456";
+accountCity = "Jaipur";
+
+// accountId = 1234;
+accountEmail = "r@gmail.com";
+accountPassword = "5678";
+accountCity = "Bengluru";
+
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity]);
