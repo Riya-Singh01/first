fec33ed8b28eeec91df1daa94219df09b923ae13
Riya singh <riyasingh56192@gmail.com>
2025-12-19 07:58:49 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; // treat all js code as newer version
+
+null // standalone value ... it is object
+Symbol //  for find uniquness
+
+console.log(typeof null) // object
+console.log(typeof undefined); //undefined
