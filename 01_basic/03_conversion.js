971c3dd48fc1036d8e76684338f840a5d8b08c93
Riya singh <riyasingh56192@gmail.com>
2026-03-03 17:48:13 +0000

    string add

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -16,6 +16,23 @@ let string1 = String(str);
 console.log(string1);
 console.log(typeof string1);
 
+let str1 ="Riya";
+let str2=" Singh"
+
+let str3=str1+str2;
+
+console.log(str3);
+
+
+let gamecounter = 100;
+// gamecounter++;
+// ++gamecounter;
+// gamecounter;
+gamecounter++;
+gamecounter;
+console.log(gamecounter);
+
+
 
 
 
